ddf010ba480c86e45fe69cc555047fdf086b9671
windripper <42181084+windripper@users.noreply.github.com>
2020-08-16 21:16:46 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'root': true,
+    'env': {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        'google'
+    ],
+    'parserOptions': {
+        parser: 'babel-eslint'
+    },
+    'rules': {
+        'object-curly-spacing': 'off',
+        'arrow-parens': 'off',
+        'comma-dangle': 'off',
+        'indent': ['error', 4],
+        'require-jsdoc': 'off',
+        'linebreak-style': 'off',
+        'operator-linebreak': 'off',
+        'no-debugger': 'warn',
+        'keyword-spacing': 'off'
+    }
+};
